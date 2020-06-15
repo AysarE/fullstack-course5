a4c405d19a831c7a73eb8c4b514fd4ad3395ce1d
Aysar Ezzour <aysare@microsoft.com>
2020-06-15 14:41:27 -0700

    Lecture 10 check-in

--- examples/Lecture10/beginning-of-lecture-code/app.js
+++ examples/Lecture10/beginning-of-lecture-code/app.js
@@ -4,6 +4,7 @@
 angular.module('DIApp', [])
 .controller('DIController', DIController);
 
+DIController.$inject = ['$scope', '$filter'];
 function DIController ($scope, $filter) {
   $scope.name = "Yaakov";
 
@@ -14,3 +15,5 @@ function DIController ($scope, $filter) {
 }
 
 })();
+
+!function(){"use strict";function e(e,n){e.name="Yaakov",e.upper=function(){var o=n("uppercase");e.name=o(e.name)}}angular.module("DIApp",[]).controller("DIController",e),e.$inject=["$scope","$filter"]}();
